7e2771b0242594baf9eade26a121904f3e4398de
Huy Tong <123931564+huysong@users.noreply.github.com>
2026-01-14 16:50:07 +0700

    feature: countExcellentStudent in StudentAnalyzer (#1)

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        int result = analyzer.countExcellentStudents(
+                Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)
+        );
+
+        System.out.println("So hoc sinh gioi: " + result);
     }
 }
